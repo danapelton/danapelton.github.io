41a01a5d751382d22d2e2824727b8dd3fa1edcc9
danapelton <danadpelton@gmail.com>
2025-04-11 15:44:59 -0700

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-
-import React from "react";
-import ReactDOM from "react-dom";
-import PersonalWebsite from "./PersonalWebsite.js";
-
-ReactDOM.render(<PersonalWebsite />, document.getElementById("root"));
