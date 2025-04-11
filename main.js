ed6449b2cc085656eba15cff007108781224e662
danapelton <danadpelton@gmail.com>
2025-04-11 16:01:10 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import PersonalWebsite from "./PersonalWebsite.js";
+
+ReactDOM.render(<PersonalWebsite />, document.getElementById("root"));
